1ee9cae75c56a8c23a7c36fad0c5a632ba7f148a
Krzysiek <50wilk>
2019-04-23 16:13:12 +0200

    duck image + duck is flying

--- src/js/GameCanvas.js
+++ src/js/GameCanvas.js
@@ -6,8 +6,14 @@ class GameCanvas extends Component{
         this.state = {
             backgroundColor: "green",
             width: 700,
-            height: 420
+            height: 420,
+            duck: {
+                x: 700,
+                y: 100,
+                speed: 2.5
+            }
         };
+        
     }
     render(){
         return (
@@ -16,25 +22,54 @@ class GameCanvas extends Component{
     }
 
     componentDidMount(){
-        this.updateCanvas(this.state);
+        this.initCanvas(this.state, this.refs.canvas.getContext('2d'));
+        requestAnimationFrame(()=>{
+            this.loop();
+        });
     }
 
     componentDidUpdate(){
         this.updateCanvas(this.state);
     }
 
-    updateCanvas(state){
-        var context = this.refs.canvas.getContext('2d');
-        
-        //context.fillStyle = this.state.backgroundColor;
-        //context.fillRect(0,0,this.state.width, this.state.height);
-        let img = new Image();
-        img.src = require("../img/background-grass.png");
-        img.onload = function() {
+    initImages(state, context){
+        this.backgroundImg = new Image();
+        this.duckImg = new Image();
+        this.backgroundImg.src = require("../img/background-grass.png");
+        this.duckImg.src = require("../img/duck.png");
+        this.backgroundImg.onload = function() {
             context.clearRect(0,0,state.width, state.height);
             context.drawImage(this, 0, 0, state.width, state.height);
         };
-        
+
+        this.duckImg.onload = function(){
+            //context.clearRect(0,0,state.width, state.height);
+            context.drawImage(this, state.width-100, 100);
+        };
+    }
+
+    initCanvas(state, context){
+        this.initImages(state, context);
+        console.log('init canvas');
+    }
+
+    updateCanvas(state){
+        //console.log('update canvas');
+        var context = this.refs.canvas.getContext('2d');
+        context.drawImage(this.backgroundImg, 0, 0);
+        context.drawImage(this.duckImg, state.duck.x, state.duck.y);
+    }
+
+    loop(){
+        //console.log('loop: ', this.state.duck);
+        this.setState((state) => ({
+            duck: {
+                x: state.duck.x > - 60 ? state.duck.x - state.duck.speed : state.width,
+                y: state.duck.y,
+                speed: state.duck.speed
+            }
+        }));
+        requestAnimationFrame(this.loop.bind(this));
     }
 
 
